d3475f4f4bd20333433ce3c9a7bca5c017b15ae9
JiangYuPeng <yupengjiang.sohu-inc.com>
2023-10-10 16:16:39 +0800

    feat: master components installation

--- master/start_master.sh
+++ master/start_master.sh
@@ -21,7 +21,6 @@
 
 # parameters 
 etcd_servers=
-etcd_prefix=/
 service_cluster_ip_range=
 cluster_dns=
 cluster_name=
@@ -39,7 +38,7 @@ APISERVER_AUTH_PATH="/etc/kubernetes/auth"
 
 
 
-print_help() {
+function print_help() {
   echo -e "
   \033[0;33mParameters explanation:
 
@@ -49,9 +48,14 @@ print_help() {
   "
 }
 
-command_exists ()
+function command_exists ()
 {
   command -v "$@" > /dev/null 2>&1
+}
+
+function service_exists ()
+{
+  systemctl list-unit-files | grep $1 > /dev/null 2>&1
 }
 
 
@@ -61,9 +65,6 @@ do
   case $arg in
     --etcd-servers=*)
       etcd_servers="${arg#*=}"
-      ;;
-    --etcd-prefix=*)
-      etcd_prefix="${arg#*=}"
       ;;
     --service-cluster-ip-range=*)
       service_cluster_ip_range="${arg#*=}"
@@ -98,6 +99,7 @@ done
 			
 
 cluster_domain=${cluster_name}.cluster.local
+etcd_prefix=/${cluster_name}/registry
 
 
 
@@ -148,10 +150,8 @@ rm -rf /tmp/master /tmp/master.tar.gz
 # STEP 04: CA
 
 mkdir -p $APISERVER_CA_PATH
-mkdir -p $APISERVER_AUTH_PATH
 
 openssl genrsa -out ca.key 2048
-
 openssl req -x509 -new -nodes -key ca.key -subj "/CN=${master_ip}" -days 36500 -out ca.crt
 
 echo "[ req ]
@@ -181,20 +181,144 @@ openssl genrsa -out apiserver.key 2048
 openssl req -new -key apiserver.key -config master_ssl.cnf -subj "/CN=${master_ip}" -out apiserver.csr
 openssl x509 -req -in apiserver.csr -CA ca.crt -CAkey ca.key -CAcreateserial -days 36500 -extensions v3_req -extfile master_ssl.cnf -out apiserver.crt
 
-mv ca.key $APISERVER_CA_PATH
-mv ca.crt $APISERVER_CA_PATH
-mv master_ssl.conf $APISERVER_CA_PATH
-mv apiserver.key $APISERVER_CA_PATH
-mv apiserver.csr $APISERVER_CA_PATH
-mv apiserver.crt $APISERVER_CA_PATH
+mv ca.key ca.crt master_ssl.conf apiserver.key apiserver.csr apiserver.crt ${APISERVER_CA_PATH}
+
+
+# STEP 05: auth token
+
+mkdir -p ${APISERVER_AUTH_PATH}
+
+token=`echo $cluster_name | base64`
+echo $token,k8s-node,0 > ${APISERVER_AUTH_PATH}/token.csv
 
 
 
 # STEP 05: start kube-apiserver
 
+echo -e "\033[36m[INFO] STEP 12: Start kube-apiserver...\033[0m"
+if service_exists kube-apiserver; then
+  systemctl stop kube-apiserver
+fi
+
+echo "[Unit]
+Description=kube-apiserver
+After=network.target
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+EnvironmentFile=/etc/sysconfig/kube-apiserver
+ExecStart=$K8S_INSTALL_PATH/current/kube-apiserver \$ETCD_SERVERS \\
+          \$SERVICE_CLUSTER_IP_RANGE \\
+          \$INSECURE_BIND_ADDRESS \\
+          \$INSECURE_PORT \\
+          \$SECURE_PORT \\
+          \$KUBE_APISERVER_OPTS
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /usr/lib/systemd/system/kube-apiserver.service
+
+
+KUBE_APISERVER_OPTS="--etcd-prefix=${etcd_prefix} --client-ca-file=${APISERVER_CA_PATH}/ca.crt --tls-private-key-file=${APISERVER_CA_PATH}/apiserver.key --tls-cert-file=${APISERVER_CA_PATH}/apiserver.crt --service-account-key-file=${APISERVER_CA_PATH}/apiserver.crt --token-auth-file=${APISERVER_AUTH_PATH}/token.csv"
+
+echo "# configure file for kube-apiserver
+
+# --etcd-servers
+ETCD_SERVERS='--etcd-servers=${etcd_servers}'
+# --service-cluster-ip-range
+SERVICE_CLUSTER_IP_RANGE='--service-cluster-ip-range=${service_cluster_ip_range}'
+# other parameters
+KUBE_APISERVER_OPTS='${KUBE_APISERVER_OPTS} --kubelet-preferred-address-types=InternalIP,Hostname,InternalDNS,ExternalDNS,ExternalIP --kubelet-client-certificate=${APISERVER_CA_PATH}/apiserver.crt --kubelet-client-key=${APISERVER_CA_PATH}/apiserver.key'
+# --insecure-bind-address
+INSECURE_BIND_ADDRESS='--insecure-bind-address=${insecure_bind_address}'
+# --insecure-port
+INSECURE_PORT='--insecure-port=${insecure_port}'
+# --secure-port
+SECURE_PORT='--secure-port=${secure_port}'
+" > /etc/sysconfig/kube-apiserver
+
+systemctl daemon-reload
+systemctl start kube-apiserver
+systemctl enable kube-apiserver
+sleep 8
+systemctl status -l kube-apiserver
+
+
 # STEP 06: start kube-controller-manager
 
+echo -e "\033[36m[INFO] STEP 13: Start kube-controller-manager...\033[0m"
+if service_exists kube-controller; then
+  systemctl stop kube-controller
+fi
+
+echo "[Unit]
+Description=kube-controller-manager
+After=kube-apiserver.service
+Wants=kube-apiserver.service
+
+[Service]
+EnvironmentFile=/etc/sysconfig/kube-controller-manager
+ExecStart=$K8S_INSTALL_PATH/current/kube-controller-manager \$KUBE_MASTER \\
+          \$KUBE_CONTROLLER_OPTS
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /usr/lib/systemd/system/kube-controller-manager.service
+
+echo "# configure file for kube-controller-manager
+
+# --master
+KUBE_MASTER='--master=http://127.0.0.1:${insecure_port}'
+
+# other parameters
+KUBE_CONTROLLER_OPTS='--root-ca-file=${APISERVER_CA_PATH}/ca.crt --service-account-private-key-file=${APISERVER_CA_PATH}/apiserver.key'
+" > /etc/sysconfig/kube-controller-manager
+
+systemctl daemon-reload
+systemctl start kube-controller
+systemctl enable kube-controller
+sleep 5
+systemctl status -l kube-controller
+
+
 # STEP 07: start kube-scheduler
+
+echo -e "\033[36m[INFO] STEP 14: Start kube-scheduler...\033[0m"
+if service_exists kube-scheduler; then
+  systemctl stop kube-scheduler
+fi
+
+echo "[Unit]
+Description=kube-scheduler
+After=kube-apiserver.service
+Wants=kube-apiserver.service
+
+[Service]
+EnvironmentFile=/etc/sysconfig/kube-scheduler
+ExecStart=$K8S_INSTALL_PATH/current/kube-scheduler \$KUBE_MASTER \\
+          \$KUBE_SCHEDULER_OPTS
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+" > /usr/lib/systemd/system/kube-scheduler.service
+
+echo "# configure file for kube-scheduler
+# --master
+KUBE_MASTER='--master=http://127.0.0.1:${insecure_port}'
+
+# other parameters
+KUBE_SCHEDULER_OPTS=''
+" > /etc/sysconfig/kube-scheduler
+
+systemctl daemon-reload
+systemctl start kube-scheduler
+systemctl enable kube-scheduler
+sleep 5
+systemctl status -l kube-scheduler
 
 
 
